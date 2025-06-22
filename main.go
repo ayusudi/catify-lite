99214801f43c23b6ca25af6f91032ef2d6679d0f
Ayu Sudi <ayusudi.abc@gmail.com>
2025-06-22 18:10:46 +0700

    update swagger

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/ayusudi/catify-lite/routes"
 )
 
-// @title Catify Lite API
+// @title Catify API
 // @version 1.0
 // @description This is an API for cat facts and comments.
 // @host localhost:8080
